672a6af3813f8691d95bd348c9a82e482028a951
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-04-04 23:20:17 +0300

    add API-data and typisation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 // @ts-ignore
 import * as bootstrap from 'bootstrap'
 import './style.scss'
+import { convertCurrency, getCurrencyList } from './api'
 
+getCurrencyList().then(list => console.log(list))
+convertCurrency('RUB', 'USD', '10').then(result => console.log(result))
